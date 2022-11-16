2747b98676dcd26645aac93389b9a8153fab05dd
Shailu <skblkg@gmail.com>
2022-11-16 09:22:47 +0530

    First Commit all pattern

---
+++ PatterninCSharp/Program.cs
@@ -0,0 +1,15 @@
+﻿
+using PatterninCSharp;
+
+
+
+Square.SquarePrint();
+
+
+#region Predefine
+
+// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+Console.ReadLine();
+
+#endregion
