7f4be3e34fb48305c1949bbdb56b7881ebf87dab
shailendrakb777 <shailendrakb777@gmail.com>
2023-01-26 12:21:00 +0530

    Add dynamic values store

---
+++ PatterninCSharp/DynamicDataStore.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatterninCSharp
+{
+    public static class DynamicDataStore
+    {
+        public static void DynamicValues(int Id)
+        {
+            Dictionary<string, object> values = new Dictionary<string, object>();
+            List<string> list = new List<string> {
+             "Shailendra",
+             "Shailendra",
+             "Shailendra1"
+        };
+            object objj = new object();
+            string dataaaa = "djhdkjfgkdf dfuisgfb snfcjsd fsnjfvdhs";
+
+            string name = "";
+
+            if (1 != Id)
+            {
+                objj = Id;
+                values.Add("Result", Convert.ToString(Id));
+            }
+            else if (1 != Id)
+            {
+                objj = list;
+                values.Add("Result", list);
+            }
+            else
+            {
+                objj = list;
+                values.Add("Result", dataaaa);
+            }
+
+        }
+
+    }
+}
--- PatterninCSharp/ReversCharchors.cs
+++ PatterninCSharp/ReversCharchors.cs
@@ -23,6 +23,8 @@ namespace PatterninCSharp
                 // reversValue += strVal[i]; we can use both
             }
             Console.WriteLine("Your num ={0}", reversValue);
+
+            
         }
         public static void ReversCharWithoutChngPosition()
         {
@@ -37,6 +39,16 @@ namespace PatterninCSharp
             Console.WriteLine(result);
 
             Console.WriteLine("one two three ={0}", result);
+
+            // second way
+            // input s= "i am developer";
+            //output s = "i ma repoleved"
+            string str = "i am developer";
+
+            string[] strArr = str.Split(' ');
+            var reverceChar = strArr.Select(x => new string(x.Reverse().ToArray()));
+            string RevResult = string.Join(" ", reverceChar);
+            Console.WriteLine(result);
         }
         public static void ReversWordWithoutChngPosition()
         {
@@ -53,6 +65,41 @@ namespace PatterninCSharp
                 
             }
             Console.Write(result);
+
+            string str = "i am developer";
+            string[] strArr = str.Split(' ');
+            Array.Reverse(strArr);
+            string res = string.Join(" ", strArr);
+            Console.WriteLine(res);
+        }
+        public static void FindDuplicate()
+        {
+            string data = "i am a developer";
+            var resw = (from obj in data select obj).GroupBy(x => x).First();
+            Console.WriteLine("tha value is {0}" + resw);
+        }
+        public static void FindPrimeNumberOrNot()
+        {
+            int num, res, reve = 0;
+            Console.WriteLine("Please Enter Number");
+            num = Convert.ToInt32(Console.ReadLine());
+            for (int i = 2; i <= num / 2; i++)
+            {
+                if (num % i == 0)
+                {
+                    reve++;
+                    break;
+                }
+            }
+            if (reve == 0 && num != 1)
+            {
+                Console.Write("{0} is prime number \n ", +num);
+            }
+            else
+            {
+                Console.Write("{0} is not Prime Number\n", +num);
+            }
+            Console.ReadLine();
         }
     }
 }
